43f835ea95fed44ce60760d8c85100926d67e9be
morgan stake <morgan.stake@cgi.com>
2018-03-21 17:59:44 -0600

    move the build artifacts Make sure we keep the creds out of version control

--- ci/tasks/struts-app/cf-push/task.sh
+++ ci/tasks/struts-app/cf-push/task.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+rm -rf ~/.aws
+mkdir ~/.aws
+chmod 700 ~/.aws
+
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id = ${AWS_ACCESS_KEY}" >> ~/.aws/credentials
+echo "aws_secret_access_key = ${AWS_SECRET_KEY}" >> ~/.aws/credentials
+
+echo "[default]" > ~/.aws/config
+echo "region = ${AWS_REGION_NAME}" >> ~/.aws/config
+echo "output = json" >> ~/.aws/config
+
 DEPLOY_FILE_NAME=$(readlink -f build-artifacts/*.war)
 
 cf login -a $CF_API -u $CF_API_USER -p $CF_API_PASSWORD
--- ci/tasks/struts-app/compile/task.sh
+++ ci/tasks/struts-app/compile/task.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+
+rm -rf ~/.aws
+mkdir ~/.aws
+chmod 700 ~/.aws
+
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id = ${AWS_ACCESS_KEY}" >> ~/.aws/credentials
+echo "aws_secret_access_key = ${AWS_SECRET_KEY}" >> ~/.aws/credentials
+
+echo "[default]" > ~/.aws/config
+echo "region = ${AWS_REGION_NAME}" >> ~/.aws/config
+echo "output = json" >> ~/.aws/config
+
+
 cd struts-examples
 mvn package
-
 if [ $? ]
 then
   for i in `find . -name "*.war"|sort|grep -v test|grep -v mailreader`
   do
-
+    echo aws s3 cp $i s3://${S3_BUCKET}/`dirname $i|cut -d'/' -f2`.war
   done
 fi
