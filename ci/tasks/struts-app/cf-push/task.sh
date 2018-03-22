a831adfd1ebfb9cf072ba922b8dea4bedd59d143
morgan stake <morgan.stake@cgi.com>
2018-03-22 16:56:56 -0600

    Formatted output

--- ci/tasks/struts-app/cf-push/task.sh
+++ ci/tasks/struts-app/cf-push/task.sh
@@ -6,14 +6,22 @@ cd ..
 
 DEPLOY_FILE_NAMES=$(find struts-app-build-artifacts -name "*.war" | sort )
 
-echo "==========================================="
+width=100
+divider="========================================================================================================================"
+divider=$divider$divider
+printf "%$width.${width}s\n" "$divider"
+
+
 echo "Deploying: "
-echo "==========================================="
+printf "%$width.${width}s\n" "$divider"
+printf "| %-30s \t|  %-69s \n" "App" "War File"
+printf "%$width.${width}s\n" "$divider"
+
 for i in $DEPLOY_FILE_NAMES
 do
-  printf "|\t App: %-25s \t|  using %-60s \n" `basename $i .war` $i
+  printf "| %-25s \t|  using %-60s \n" `basename $i .war` $i
 done
-echo "==========================================="
+printf "%$width.${width}s\n" "$divider"
 
 cf login --skip-ssl-validation -o ${CF_ORG} -s ${CF_SPACE} -a $CF_API -u $CF_API_USER -p $CF_API_PASSWORD
 for i in $DEPLOY_FILE_NAMES
