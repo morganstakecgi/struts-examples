be1365e2fdd9a171b17e465fe988affcf0d442e5
morgan stake <morgan.stake@cgi.com>
2018-03-22 08:46:33 -0600

    pass artifacts

--- ci/tasks/struts-app/compile/task.sh
+++ ci/tasks/struts-app/compile/task.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-#
-#rm -rf ~/.aws
-#mkdir ~/.aws
-#chmod 700 ~/.aws
-#
-#echo "[default]" > ~/.aws/credentials
-#echo "aws_access_key_id = ${AWS_ACCESS_KEY}" >> ~/.aws/credentials
-#echo "aws_secret_access_key = ${AWS_SECRET_KEY}" >> ~/.aws/credentials
-#
-#echo "[default]" > ~/.aws/config
-#echo "region = ${AWS_REGION_NAME}" >> ~/.aws/config
-#echo "output = json" >> ~/.aws/config
-
 mkdir -p build_artifacts
 cd struts-examples
 mvn package -pl annotations -am
@@ -21,8 +8,6 @@ if [ $? ]
 then
   for i in `find . -name "*.war"|sort|grep -v test|grep -v mailreader`
   do
-#    echo aws s3 cp $i s3://${S3_BUCKET}/`dirname $i|cut -d'/' -f2`.war
-#    aws s3 cp $i s3://${S3_BUCKET}/`dirname $i|cut -d'/' -f2`.war
       cp $i ../build_artifacts/`dirname $i|cut -d'/' -f2`.war
   done
 fi
