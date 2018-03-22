f9471422540e67cbd66b665a3d01044e8a55829c
morgan stake <morgan.stake@cgi.com>
2018-03-22 09:26:08 -0600

    Deploy everything!

--- ci/tasks/struts-app/cf-push/task.sh
+++ ci/tasks/struts-app/cf-push/task.sh
@@ -4,6 +4,8 @@ DEPLOY_FILE_NAME=$(readlink -f build-artifacts/*.war)
 
 cf login --skip-ssl-validation -o ${CF_ORG} -s ${CF_SPACE} -a $CF_API -u $CF_API_USER -p $CF_API_PASSWORD
 
+echo "Deploying: " $DEPLOY_FILE_NAME
+
 for i in $DEPLOY_FILE_NAME
 do
   CMD="cf push -m750m `basename $i .war` -p $i"
