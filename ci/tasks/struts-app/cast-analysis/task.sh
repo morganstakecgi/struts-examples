52260a2470dcf871990eda6ebdb010a7c4d710c7
morgan stake <morgan.stake@cgi.com>
2018-03-28 16:06:41 -0600

    ADDED CAST ANALYSIS... better name and s3 bucket

--- ci/tasks/struts-app/cast-analysis/task.sh
+++ ci/tasks/struts-app/cast-analysis/task.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 WORK_DIR=`pwd`/struts-app-cast-artifacts
-TAR_FILE=$WORK_DIR/struts-examples-cast-analysis.tar.gz
+TAR_FILE=struts-examples-cast-analysis.tar.gz
 
 mkdir -p $WORK_DIR
 cd struts-examples
 java -jar ${CAST_AGENT_JAR} --analyzerDir  ${CAST_AGENT_PERL} --workingDir ${WORK_DIR} --sourceDir . --skipUpload
-cd ${WORK_DIR}
-tar -zcvf $TAR_FILE `find .`
+tar -zcvf $TAR_FILE `find .` && mv ${TAR_FILE} $WORK_DIR
 
 
 #java -jar /Users/morgan.stake/highlight/Highlight-Automation-Command/HighlightAutomation.jar --analyzerDir  “<path to the untarred file>/perl” --workingDir "/Users/morgan.stake/highlight.work" --sourceDir /Users/morgan.stake/git/spring-music  --login "morgan.stake@cgi.com --password "***********" --applicationId 1234 --companyId 1234 --serverUrl "https://rpa.casthighlight.com"
